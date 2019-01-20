db0966f6148f38dad5f24e21e4cfc582b704245f
Road to Guru <33366397+road2guru@users.noreply.github.com>
2019-01-20 16:46:53 +0700

    Update cloud9-installer.sh

--- cloud9-installer.sh
+++ cloud9-installer.sh
@@ -1,70 +1,70 @@
 #!/bin/bash
 
-USR_DIR=${1:-$HOME}/c9sdk
-BIN_DIR=/home/sdao/apps/c9ide-devel/.c9
+INSTALL_PATH=${1:-$HOME}/c9sdk
+BIN_PATH=$INSTALL_PATH/.c9
+LOG_FILE=installation.log
 
-echo -n "Enter installation path, default ($USR_DIR): "
-read install_dir
-if [ -d "$install_dir/c9sdk" ]
+echo -n "Enter installation path, default ($INSTALL_PATH): "
+read custom_path
+if [ -d "$custom_path" ]
 then
-	USR_DIR=$install_dir
+	INSTALL_PATH=$custom_path
 fi
-
-export INSTALL_DIR=$USR_DIR
-echo "=== install dir $INSTALL_DIR"
+	
+echo "=== install path $INSTALL_PATH"
 
 #
 # get the core path
 #
-if [ -d "$INSTALL_DIR" ]; then
-	git reset --hard
-	git fetch
-else
-	git clone https://github.com/c9/core.git $INSTALL_DIR
+if [ ! -d "$INSTALL_PATH" ]; then
+	git clone https://github.com/c9/core.git $INSTALL_PATH >> $LOG_FILE
 fi
+
+(
+    cd $INSTALL_PATH
+    git reset --hard  >> $LOG_FILE
+    git fetch >> $LOG_FILE
+)
 
 #
 # install scripts, by default set th install path to ~/.c9 - let's change it
 #
-cd $INSTALL_DIR
-
-git reset --hard	
-	
-vim -c '%s/ bash\>/ bash -s - -d $BIN_DIR/g | wq' $INSTALL_DIR/scripts/install-sdk.sh
-vim -c '%s/\~/$INSTALL_DIR/g | wq' $INSTALL_DIR/scripts/install-sdk.sh
-vim -c '%s/C9_DIR="$HOME"/C9_DIR="$INSTALL_DIR"/g | wq' $INSTALL_DIR/scripts/install-sdk.sh
-
-cat $INSTALL_DIR/scripts/install-sdk.sh
-
+vim -c '%s/ bash\>/ bash -s - -d $BIN_PATH/g | wq'          $INSTALL_PATH/scripts/install-sdk.sh
+vim -c '%s/\~/$INSTALL_PATH/g | wq'                         $INSTALL_PATH/scripts/install-sdk.sh
+vim -c '%s/C9_DIR="$HOME"/C9_DIR="$INSTALL_PATH"/g | wq'    $INSTALL_PATH/scripts/install-sdk.sh
+vim -c '%s/updateCore /\# updateCore /g | wq'               $INSTALL_PATH/scripts/install-sdk.sh
 
 #
 # start installation
 #
-$INSTALL_DIR/scripts/install-sdk.sh
+if [ $? -eq 0 ]; then
+    BIN_PATH=$BIN_PATH INSTALL_PATH=$INSTALL_PATH $INSTALL_PATH/scripts/install-sdk.sh >> $LOG_FILE
+else
+    echo "Error! unable to continue! check $LOG_FILE for more detail."
+fi
 
 #
 # some scripts, default path was set to ~/.c9. This is annoying, let's change it here
 #
-for i in `egrep -l "env\.HOME\>" $INSTALL_DIR -R | grep "\.js\$"`
+for i in `egrep -l "env\.HOME\>" $INSTALL_PATH -R | grep -E '\.js$|\.sh$|\.rc$'`
 do
-    rm -f "$i.swp"
+    echo $i
+    rm -f "$(dirname ${i}.swp)*.swp"
 	vim -c '%s/env.HOME\>/env.C9_DIR/g | wq' $i
 done
 
-cat << EOF > bin/run.sh
+cat << EOF > $INSTALL_PATH/bin/run.sh
 #!/bin/bash
 
-export PATH=$BIN_DIR/bin:$BIN_DIR/node/bin:$BIN_DIR/node_modules/.bin:$PATH
-export C9_DIR="$INSTALL_DIR"
+export PATH=$BIN_PATH/bin:$BIN_PATH/node/bin:$BIN_PATH/node_modules/.bin:$PATH
+export C9_DIR="$INSTALL_PATH"
 
-node $INSTALL_DIR/server.js -p 8181 ${@:2}
+node $INSTALL_PATH/server.js \${@:1}
 
 EOF
-chmod 755 bin/run.sh
+chmod 755 $INSTALL_PATH/bin/run.sh
 
 echo "===================================================================="
-echo "done."
+echo "Installation completed."
+echo "Start cloud9 by: $INSTALL_PATH/bin/run.sh [option]"
 echo "===================================================================="
-
-cd -
-
